725f8e22bbee3cea540f82bd700d8559dd3a0737
Lucas Oliveira <lucaspwo@gmail.com>
2018-09-18 00:29:58 -0300

    PU1+botão liga

--- Projeto_Final_U1/Projeto_U1/Projeto_U1.ino
+++ Projeto_Final_U1/Projeto_U1/Projeto_U1.ino
@@ -8,6 +8,7 @@ int cont = 0;               //inicializacao da variavel que vai contar os overfl
 float ldrLum;             //variavel responsavel por armazenar a taxa de luz
 uint16_t amostras[5];     //variavel para armazenar 5 leituras consecutivas para calculo de media
 float media;              //variavel para armazenar a media
+boolean inicio = false;
 
 ISR(TIMER1_OVF_vect) {      //funcao que e chamada a cada overflow do TIMER1
   cont++;
@@ -74,7 +75,7 @@ void readDHT() {
   Serial.print(".");
   Serial.print(dht11_dat[3], DEC);
   Serial.println("C  ");
-  delay(2000);  //fresh time
+  delay(1500);  //fresh time
 }
 
 void setup() {
@@ -84,6 +85,7 @@ void setup() {
 
   ADCSRA |= 0b10000111;     //setando o ADEN em 1 e o prescaler em 111 (ADPS1..0)
   DDRB |= _BV(DDB5);        //setando o pino B5 como saida (pino 13 no Arduino)
+  DDRB &= ~(1 << PB4);
 
   sei();                    //ativando interrupcoes
 
@@ -95,52 +97,65 @@ void setup() {
 }
 
 void loop() {
-
-  for (uint8_t i = 0; i < 5; i++) {
-    ADMUX &= 0;                                   //zerando ADMUX
-    ADMUX |= 0b01000000;                          //setando o AREF para 5V e setando MUX3..0 para ler o ADC0 (A0 no Arduino)
-    ADCSRA |= 0b01000000;                         //iniciando a conversao
-    while (!(ADCSRA & 0b00010000));               //esperando pelo ADIF = 1
-    amostras[i] = ADC;                            //armazenando o resultado
+  if ((PINB & (1 << PB4))) {
+    inicio = true;
+    PORTB |= _BV(PORTB5);
+    //    Serial.print("inicio = ");
+    //    Serial.println(inicio);
   }
 
-  media = 0;
-  for (uint8_t i = 0; i < 5; i++) {               //tirando a media dos valores
-    media += amostras[i];
-  }
-  media /= 5;
-
-  ldrLum = media;                                 //convertendo de nivel para "lumens"
-  ldrLum = 4 * ldrLum + 1000;
-
-  readDHT();
-
-  /*for(uint8_t i = 0; i<5; i++){
-    ADMUX &= 0;                                   //zerando ADMUX
-    ADMUX |= 0b01000001;                          //setando o AREF para 5V e setando MUX3..0 para ler o ADC1 (A1 no Arduino)
-    ADCSRA |= 0b01000000;                         //iniciando a conversao
-    while(!(ADCSRA & 0b00010000));                //esperando pelo ADIF = 1
-    amostras[i] = ADC;                            //armazenando o resultado
+  if (inicio) {
+    for (uint8_t i = 0; i < 5; i++) {
+      ADMUX &= 0;                                   //zerando ADMUX
+      ADMUX |= 0b01000000;                          //setando o AREF para 5V e setando MUX3..0 para ler o ADC0 (A0 no Arduino)
+      ADCSRA |= 0b01000000;                         //iniciando a conversao
+      while (!(ADCSRA & 0b00010000));               //esperando pelo ADIF = 1
+      amostras[i] = ADC;                            //armazenando o resultado
     }
 
     media = 0;
-    for(uint8_t i = 0; i<5; i++){                   //tirando a media dos valores
-    media += amostras[i];
+    for (uint8_t i = 0; i < 5; i++) {               //tirando a media dos valores
+      media += amostras[i];
     }
     media /= 5;
 
-    media = 1023 / media -1;                        //obtendo a resistencia medida pelo sensor
-    media = 4700 / media;
+    ldrLum = media;                                 //convertendo de nivel para "lumens"
+    ldrLum = 4 * ldrLum + 1000;
 
-    steinhart = media / 4700;                       //realizando o calculo de conversao de resistencia em graus celsius
-    steinhart = log(steinhart);
-    steinhart /= 3950;
-    steinhart += 1.0 / (25 + 273.15);
-    steinhart = 1.0 / steinhart;
-    steinhart -= 273.15;*/
+    readDHT();
 
-  /*if((ldrLum >= 3500 && steinhart >= 25) || cont >= 7)      //condicao de acionamento do LED
-    PORTB |= _BV(PORTB5);                                   //acionando o LED no pino B5
-    else
-    PORTB &= ~_BV(PORTB5);                                  //desligando o LED no pino B5*/
+    if ((PINB & (1 << PB4))) {
+      inicio = false;
+      PORTB &= ~_BV(PORTB5);
+    }
+
+    /*for(uint8_t i = 0; i<5; i++){
+      ADMUX &= 0;                                   //zerando ADMUX
+      ADMUX |= 0b01000001;                          //setando o AREF para 5V e setando MUX3..0 para ler o ADC1 (A1 no Arduino)
+      ADCSRA |= 0b01000000;                         //iniciando a conversao
+      while(!(ADCSRA & 0b00010000));                //esperando pelo ADIF = 1
+      amostras[i] = ADC;                            //armazenando o resultado
+      }
+
+      media = 0;
+      for(uint8_t i = 0; i<5; i++){                   //tirando a media dos valores
+      media += amostras[i];
+      }
+      media /= 5;
+
+      media = 1023 / media -1;                        //obtendo a resistencia medida pelo sensor
+      media = 4700 / media;
+
+      steinhart = media / 4700;                       //realizando o calculo de conversao de resistencia em graus celsius
+      steinhart = log(steinhart);
+      steinhart /= 3950;
+      steinhart += 1.0 / (25 + 273.15);
+      steinhart = 1.0 / steinhart;
+      steinhart -= 273.15;*/
+
+    /*if((ldrLum >= 3500 && steinhart >= 25) || cont >= 7)      //condicao de acionamento do LED
+      PORTB |= _BV(PORTB5);                                   //acionando o LED no pino B5
+      else
+      PORTB &= ~_BV(PORTB5);                                  //desligando o LED no pino B5*/
+  }
 }
