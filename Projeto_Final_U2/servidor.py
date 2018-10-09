97f73946a63f11d98f691cc19a15c9fc1043c0f2
Lucas Oliveira <lucaspwo@gmail.com>
2018-10-08 23:30:54 -0300

    Código 99,9% concluído
    Faltou apenas um comando para terminar a execução das threads

--- Projeto_Final_U2/servidor.py
+++ Projeto_Final_U2/servidor.py
@@ -1,15 +1,15 @@
-import threading, socket, mraa, time
+import threading, socket, mraa, time, os
 
 i2c = mraa.I2c(0)
 i2c.address(0x04)
 d3 = mraa.Pwm(3)        #L2
-d3.period_us(100)       #10kHz
+d3.period_us(700)
 d5 = mraa.Pwm(5)        #L3
-d5.period_us(100)       #10kHz
+d5.period_us(700)
 d6 = mraa.Pwm(6)        #DRV
-d6.period_us(100)       #10kHz
-d12 = mraa.Gpio(12)     #L1
-d12.dir(mraa.DIR_OUT)
+d6.period_us(700)
+d8 = mraa.Gpio(8)       #L1
+d8.dir(mraa.DIR_OUT)
 a1 = mraa.Aio(1)        #CH1
 a0 = mraa.Aio(0)        #LDR
 
@@ -31,11 +31,23 @@ conectado = True
 global init
 init = False
 
+global act
+act = False
+
 global temp
 temp = 0
 
 global umi
 umi = 0
+
+global luz
+luz = 0
+
+global curva
+curva = 0
+
+global tempo
+tempo = 0
 
 class recebeMsgCliente(threading.Thread):
     def __init__(self,clientes,chave):
@@ -44,26 +56,67 @@ class recebeMsgCliente(threading.Thread):
         self.chave = chave
     def run(self):
         conectado = True
+        global init
+        global act
         while conectado:
             if self.chave in self.clientes:
                 try:
                     msg = self.clientes[self.chave]['socket'].recv(2048)
                     if msg == 'iniciar()':
-                        global init
                         init = True
+                        act = False
                         print('Iniciando processo de secagem')
                         self.clientes[self.chave]['socket'].send('Iniciando processo de secagem...\n')
                         thread_enviarMsgInicio = enviaMsgCliente(self.clientes,'Iniciando processo de secagem',self.chave)
                         thread_enviarMsgInicio.start()
                     elif msg == 'terminar()':
-                        global init
                         init = False
+                        act = False
                         print('Terminando o processo de secagem')
                         self.clientes[self.chave]['socket'].send('Terminando o processo de secagem...\n')
                         thread_enviarMsgTermina = enviaMsgCliente(self.clientes,'Terminando o processo de secagem',self.chave)
                         thread_enviarMsgTermina.start()
+                    elif msg == 'curva1()':
+                        act = True
+                        init = True
+                        print('Forcando a curva 1 de secagem')
+                        threadCurva1.start()
+                        self.clientes[self.chave]['socket'].send('Forcando a curva 1 de secagem...\n')
+                        thread_enviarMsgTermina = enviaMsgCliente(self.clientes,'Forcando a curva 1 de secagem',self.chave)
+                        thread_enviarMsgTermina.start()
+                    elif msg == 'curva2()':
+                        act = True
+                        init = True
+                        print('Forcando a curva 2 de secagem')
+                        threadCurva2.start()
+                        self.clientes[self.chave]['socket'].send('Forcando a curva 2 de secagem...\n')
+                        thread_enviarMsgTermina = enviaMsgCliente(self.clientes,'Forcando a curva 2 de secagem',self.chave)
+                        thread_enviarMsgTermina.start()
+                    elif msg == 'curva3()':
+                        act = True
+                        init = True
+                        print('Forcando a curva 3 de secagem')
+                        threadCurva3.start()
+                        self.clientes[self.chave]['socket'].send('Forcando a curva 3 de secagem...\n')
+                        thread_enviarMsgTermina = enviaMsgCliente(self.clientes,'Forcando a curva 3 de secagem',self.chave)
+                        thread_enviarMsgTermina.start()
+                    elif msg == 'curva4()':
+                        act = True
+                        init = True
+                        print('Forcando a curva 4 de secagem')
+                        threadCurva4.start()
+                        self.clientes[self.chave]['socket'].send('Forcando a curva 4 de secagem...\n')
+                        thread_enviarMsgTermina = enviaMsgCliente(self.clientes,'Forcando a curva 4 de secagem',self.chave)
+                        thread_enviarMsgTermina.start()
+                    elif msg == 'curva5()':
+                        act = True
+                        init = True
+                        print('Forcando a curva 5 de secagem')
+                        threadCurva5.start()
+                        self.clientes[self.chave]['socket'].send('Forcando a curva 5 de secagem...\n')
+                        thread_enviarMsgTermina = enviaMsgCliente(self.clientes,'Forcando a curva 5 de secagem',self.chave)
+                        thread_enviarMsgTermina.start()
                     elif msg == 'sair()':
-                        global init
                         init = False
                         self.clientes[self.chave]['socket'].send('Saindo do servidor...')
                         self.clientes[self.chave]['socket'].close()
@@ -116,15 +169,19 @@ class servidor(threading.Thread):
                 listen_socket.close()
                 conectado = False
                 # sys.exit(1)
+                os._exit(1)
 
 class botao(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
     def run(self):
+        global init
+        global act
         while True:
             if(a1.read() == False):
-                global init
-                init = True
+                if (init == False):
+                    init = True
+                    act = False
 
 class ledL1(threading.Thread):
     def __init__(self):
@@ -133,9 +190,9 @@ class ledL1(threading.Thread):
         while True:
             global init
             if init:
-                d12.write(1)
+                d8.write(1)
             else:
-                d12.write(0)
+                d8.write(0)
 
 class ledLDR(threading.Thread):
     def __init__(self):
@@ -146,6 +203,8 @@ class ledLDR(threading.Thread):
             if init:
                 d3.enable(True)
                 d3.write(a0.readFloat())
+                global luz
+                luz = a0.read()
             else:
                 d3.enable(False)
 
@@ -157,28 +216,293 @@ class readI2c(threading.Thread):
             global temp
             global umi
             temp = int(i2c.readByte())
+            #print('Temp: %d' % temp)
             umi = int(i2c.readByte())
+            #print('Umi: %d' % umi)
             time.sleep(2)
+
+class conta(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        global tempo
+        while True:
+            tempo = time.time()
+
+class curva1(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        print('Iniciando curva 1')
+        d5.enable(True)
+        d6.enable(True)
+        global tempo
+        global init
+        pwm = 0
+        d5.write(pwm)
+        d6.write(pwm)
+        print('pwm: %f' % pwm)
+        for i in range(10):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm + 0.035
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        agora = tempo
+        while(tempo - agora < 5):
+            pass
+        for i in range(5):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm + 0.06
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        agora = tempo
+        while(tempo - agora < 5):
+            pass
+        for i in range(5):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm - 0.13
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        d5.enable(False)
+        d6.enable(False)
+        init = False
+
+class curva2(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        print('Iniciando curva 2')
+        d5.enable(True)
+        d6.enable(True)
+        global tempo
+        global init
+        pwm = 0
+        d5.write(pwm)
+        d6.write(pwm)
+        print('pwm: %f' % pwm)
+        for i in range(20):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm + 0.035
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        agora = tempo
+        while(tempo - agora < 5):
+            pass
+        for i in range(5):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm + 0.06
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        agora = tempo
+        while(tempo - agora < 5):
+            pass
+        for i in range(10):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm - 0.10
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        d5.enable(False)
+        d6.enable(False)
+        init = False
+
+class curva3(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        print('Iniciando curva 3')
+        d5.enable(True)
+        d6.enable(True)
+        global tempo
+        global init
+        pwm = 0
+        d5.write(pwm)
+        d6.write(pwm)
+        print('pwm: %f' % pwm)
+        for i in range(10):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm + 0.035
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        agora = tempo
+        while(tempo - agora < 10):
+            pass
+        for i in range(10):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm + 0.025
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        agora = tempo
+        while(tempo - agora < 10):
+            pass
+        for i in range(10):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm - 0.06
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        d5.enable(False)
+        d6.enable(False)
+        init = False
+
+class curva4(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        print('Iniciando curva 4')
+        d5.enable(True)
+        d6.enable(True)
+        global tempo
+        global init
+        pwm = 0
+        d5.write(pwm)
+        d6.write(pwm)
+        print('pwm: %f' % pwm)
+        for i in range(10):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm + 0.055
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        agora = tempo
+        while(tempo - agora < 5):
+            pass
+        for i in range(10):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm + 0.045
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        agora = tempo
+        while(tempo - agora < 10):
+            pass
+        for i in range(10):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm - 0.10
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        d5.enable(False)
+        d6.enable(False)
+        init = False
+
+class curva5(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        print('Iniciando curva 5')
+        d5.enable(True)
+        d6.enable(True)
+        global tempo
+        global init
+        pwm = 0
+        d5.write(pwm)
+        d6.write(pwm)
+        print('pwm: %f' % pwm)
+        for i in range(10):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm + 0.035
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        agora = tempo
+        while(tempo - agora < 5):
+            pass
+        for i in range(5):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm + 0.04
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        agora = tempo
+        while(tempo - agora < 10):
+            pass
+        for i in range(20):
+            agora = tempo
+            while(tempo - agora < 1):
+                pass
+            pwm = pwm - 0.0275
+            d5.write(pwm)
+            d6.write(pwm)
+            print('pwm: %f' % pwm)
+        d5.enable(False)
+        d6.enable(False)
+        init = False
 
 class secador(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
     def run(self):
-        
-        d5.enable(True)
-        d6.enable(True)
-        
         while True:
             global init
-            while init:
-                luz = a0.read()
-                #receber valor DHT do Arduino (i2c)
-                
+            global act
+            if ( (init == True) and (act == False) ):
+                global temp
+                global umi
+                global luz
+                global curva
+                if( (temp > 20 and temp <= 50) and (umi > 80)  and (luz > 500 and luz <= 800) ):
+                    curva = 1
+                    act = True
+                    threadCurva1.start()
+                elif( (temp > 20 and temp <= 50) and (umi <= 70) and (luz > 500 and luz <= 800) ):
+                    curva = 2
+                    act = True
+                    threadCurva2.start()
+                elif( (temp > 20 and temp <= 50) and (umi <= 70) and (luz > 200 and luz <= 500) ):
+                    curva = 3
+                    act = True
+                    threadCurva3.start()
+                elif( (temp > 50 and temp <= 80) and (umi <= 60) and (luz > 200 and luz <= 700) ):
+                    curva = 4
+                    act = True
+                    threadCurva4.start()
+                else:
+                    curva = 5
+                    act = True
+                    threadCurva5.start()
 
 clientes = {}
 
 threadServ = servidor(clientes)
 threadServ.start()
+threadConta = conta()
+threadConta.start()
 threadBotao = botao()
 threadBotao.start()
 threadLedL1 = ledL1()
@@ -187,6 +511,11 @@ threadLedLDR = ledLDR()
 threadLedLDR.start()
 threadReadI2c = readI2c()
 threadReadI2c.start()
+threadCurva1 = curva1()
+threadCurva2 = curva2()
+threadCurva3 = curva3()
+threadCurva4 = curva4()
+threadCurva5 = curva5()
 threadSecador = secador()
 threadSecador.start()
 
@@ -204,6 +533,7 @@ while conectado:
             client_connection.send('Servidor: Voce esta conectado\n\n')
             client_connection.send('Comandos:\n')
             client_connection.send('iniciar()\t: Inicia a secagem de graos\n')
+            client_connection.send('curva#()\t: Forca uma curva de secagem (#: 1 a 5)\n')
             client_connection.send('terminar()\t: Termina a secagem de graos\n')
             client_connection.send('sair()\t\t: Desconecta do servidor\n')
             threadRecebe = recebeMsgCliente(clientes,client_address)
